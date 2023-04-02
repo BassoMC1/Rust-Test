3734b6d6c8d9b5106bd88863a1dffdc32516011b
BassoMC <Bassow05@gmail.com>
2023-04-02 21:20:36 +0200

    made a webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 fn main() {
     let mut x = 4;
     println!("x is: {}", x);
@@ -56,4 +61,35 @@ fn main() {
     // char can only store ting ting 
     let letter: char = 'a';
     println!("letter is: {}", letter);
+
+    //made a webserver with rust that will render a html file and it wil run on port 7878
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+
+
+
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let _http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+    let file_path = "src/hello.html";
+    let contents = fs::read_to_string(file_path).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
